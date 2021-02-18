d1d15cdac85fcd92e3e63cade8574c00a5ce6584
kylescoulter <50339406+kylescoulter@users.noreply.github.com>
2021-02-18 02:23:11 -0600

    Added icons to buttons in RecordContainer.

--- app/src/components/RecordContainer.tsx
+++ app/src/components/RecordContainer.tsx
@@ -1,5 +1,5 @@
 import './RecordContainer.css';
-import { camera, trash, close } from 'ionicons/icons';
+import {camera, trash, close, hourglass, hourglassOutline, golf} from 'ionicons/icons';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
     IonFab, IonButton, IonIcon, IonGrid, IonRow,
     IonCol, IonImg, IonActionSheet } from '@ionic/react';
@@ -14,11 +14,13 @@ const RecordContainer: React.FC<ContainerProps> = () => {
     <div className="recording">
         <div>
             <IonButton shape='round' size='large' color='success' mode={'ios'} onClick={() => doSinglePutt()}>
+                <IonIcon icon={golf} />
                 SINGLE
             </IonButton>
         </div>
         <div>
             <IonButton shape='round' size='large' color='dark' mode={'ios'} onClick={() => doTimelessPutts()}>
+                <IonIcon icon={hourglassOutline} />
                 ENDLESS
             </IonButton>
         </div>
